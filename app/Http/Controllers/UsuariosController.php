9376f521dc929eb10ced10a24e775f0995c4c09b
WesleiRafael <rafael_velao@hotmail.com>
2022-11-17 20:03:06 -0300

    alteração das classes models e controller

--- app/Http/Controllers/UsuariosController.php
+++ app/Http/Controllers/UsuariosController.php
@@ -2,84 +2,95 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Usuario;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 
 class UsuariosController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        // $usuarios = DB::table('users')->select('id', 'name', 'email')->get();
-        $usuarios = Usuario::all();
-        return response()->json($usuarios, 200);
+    public function index() {
+        $usuarios = DB::table('usuarios')->get();
+
+        return view('usuarios.listar', ['usuarios' => $usuarios]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
+    public function show($id) {
+        $usuario = DB::table('usuarios')->where('id', $id)->first();
+
+        return view('usuarios.detalhes', ['usuario' => $usuario]);
+    }
+
+    public function create() {
+        return view('usuarios.novo');
+    }
+
+    public function store(Request $request) {
         $validated = Validator::make($request->all(), [
-            'name'     => 'required',
-            'email'    => 'required|email',
-            'password' => 'required',
-        ]);
+            'Nome'  => 'required|min:3|max:128',
+            'Email' => 'required|email|min:0',
+            'Idade' => 'required|integer|min:0|not_in:0',
+            'Telefone' => 'required|numeric|min:0'
+        ], [], ['Nome' => 'Nome', 'Email' => 'Email', 'Idade' => 'Idade', 'Telefone' => 'Telefone']);
 
-        if ($validated->fails()) {
-            return response()->json(['mensagem' => 'Falhou!'], 422);
+        if($validated->fails()) {
+            return redirect('usuarios/novo')->withErrors($validated)->withInput();
+        } else {
+            DB::table('usuarios')->insert([
+                'Nome'  => $request->Nome,
+                'Email' => $request->Email,
+                'Idade' => $request->Idade,
+                'Telefone' => $request->Telefone,
+            ]);
+
+            return redirect('usuarios')->with('mensagem', 'Usuario cadastrado.');
         }
-
-        $data = [
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => $request->password,
-        ];
-
-        Usuario::create($data);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+    public function edit($id)
     {
-        $usuario = Usuario::find($id);
-        return response()->json($usuario, 200);
+        if (! DB::table('usuarios')->where('id', $id)->first()) {
+            return redirect('usuarios')->with('mensagem', 'Usuario não encontrado.');
+        }
+        
+        $usuario = DB::table('usuarios')->where('id', $id)->first();
+        return view('usuarios.editar', ['usuario' => $usuario]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
-        //
+        if (! DB::table('usuarios')->where('id', $id)->first()) {
+            return redirect('usuarios')->with('mensagem', 'Usuario não encontrado.');
+        }
+
+        $validated = Validator::make($request->all(), [
+            'Nome'  => 'required|min:3|max:120',
+            'Email' => 'required|email|min:0',
+            'Idade' => 'required|integer|min:0|not_in:0',
+            'Telefone' => 'required|numeric|min:0'
+        ]);
+
+        if($validated->fails()) {
+            return redirect('usuarios/editar/'.$id)->withErrors($validated);
+        } else {
+            $usuario = [
+                'Nome'  => $request->Nome,
+                'Email' => $request->Email,
+                'Idade' => $request->Idade,
+                'Telefone' => $request->Telefone,
+            ];
+
+            DB::table('usuarios')->where('id', $id)->update($usuario);
+            return redirect('usuarios')->with('mensagem', 'Usuario alterado.');
+        }
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
-        //
+        if (! DB::table('usuarios')->where('id', $id)->first()) {
+            return redirect('usuarios')->with('mensagem', 'Usuario não encontrado.');
+        }
+
+        DB::table('usuarios')->where('id', $id)->delete();
+        return redirect('usuarios')->with('mensagem', 'Usuario excluído.');
     }
 }
